3505bc86cca351d1c9b966bf8fa6edfd0ac2fc3f
Proflylab <proflylab@protonmail.com>
2018-03-18 01:24:39 +0700

    First Release

---
+++ Mika/frmMain.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace Mika
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lvListW = new System.Windows.Forms.ListView();
+            this.colTitle = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.colHwnd = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.btnAll = new System.Windows.Forms.Button();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.btnSelect = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lvListW
+            // 
+            this.lvListW.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.colTitle,
+            this.colHwnd});
+            this.lvListW.FullRowSelect = true;
+            this.lvListW.Location = new System.Drawing.Point(12, 12);
+            this.lvListW.Name = "lvListW";
+            this.lvListW.Size = new System.Drawing.Size(428, 261);
+            this.lvListW.TabIndex = 0;
+            this.lvListW.UseCompatibleStateImageBehavior = false;
+            this.lvListW.View = System.Windows.Forms.View.Details;
+            this.lvListW.DoubleClick += new System.EventHandler(this.lvListW_DoubleClick);
+            // 
+            // colTitle
+            // 
+            this.colTitle.Text = "Window Title";
+            this.colTitle.Width = 320;
+            // 
+            // colHwnd
+            // 
+            this.colHwnd.Text = "";
+            this.colHwnd.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.colHwnd.Width = 80;
+            // 
+            // btnAll
+            // 
+            this.btnAll.Location = new System.Drawing.Point(446, 12);
+            this.btnAll.Name = "btnAll";
+            this.btnAll.Size = new System.Drawing.Size(75, 23);
+            this.btnAll.TabIndex = 1;
+            this.btnAll.Text = "ALL";
+            this.btnAll.UseVisualStyleBackColor = true;
+            this.btnAll.Click += new System.EventHandler(this.btnAll_Click);
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Location = new System.Drawing.Point(446, 70);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(75, 23);
+            this.btnRefresh.TabIndex = 3;
+            this.btnRefresh.Text = "Refresh";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
+            // 
+            // btnSelect
+            // 
+            this.btnSelect.Location = new System.Drawing.Point(446, 41);
+            this.btnSelect.Name = "btnSelect";
+            this.btnSelect.Size = new System.Drawing.Size(75, 23);
+            this.btnSelect.TabIndex = 2;
+            this.btnSelect.Text = "Selected";
+            this.btnSelect.UseVisualStyleBackColor = true;
+            this.btnSelect.Click += new System.EventHandler(this.btnSelect_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(528, 282);
+            this.Controls.Add(this.btnSelect);
+            this.Controls.Add(this.btnRefresh);
+            this.Controls.Add(this.btnAll);
+            this.Controls.Add(this.lvListW);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Mika - Window Center 1.0";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView lvListW;
+        private System.Windows.Forms.ColumnHeader colTitle;
+        private System.Windows.Forms.ColumnHeader colHwnd;
+        private System.Windows.Forms.Button btnAll;
+        private System.Windows.Forms.Button btnRefresh;
+        private System.Windows.Forms.Button btnSelect;
+    }
+}
+
---
+++ Mika/frmMain.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+namespace Mika
+{
+    public partial class frmMain : Form
+    {
+
+        #region WindowAPI
+        [DllImport("user32.dll", EntryPoint = "SetWindowPos")]
+        private static extern IntPtr SetWindowPos(IntPtr hWnd, int hWndInsertAfter, int x, int Y, int cx, int cy, int wFlags);
+        [DllImport("user32.dll")]
+        private static extern bool GetWindowRect(IntPtr hWnd, out RECT lpRect);
+        private struct RECT
+        {
+            public int Left;
+            public int Top;
+            public int Right;
+            public int Bottom;
+        }
+        #endregion
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private Size GetControlSize(IntPtr hWnd)
+        {
+            RECT pRect;
+            Size cSize = new Size();
+            // get coordinates relative to window
+            GetWindowRect(hWnd, out pRect);
+
+            cSize.Width = pRect.Right - pRect.Left;
+            cSize.Height = pRect.Bottom - pRect.Top;
+
+            return cSize;
+        }
+
+        private void GetList()
+        {
+            string[] defBlist = { "devenv", "ShellExperienceHost" };
+            lvListW.Items.Clear();
+            Process[] processes = Process.GetProcesses();
+            foreach (var process in processes)
+            {
+                var proc = process.ProcessName;
+                var hwnd = process.MainWindowHandle;
+                var winn = process.MainWindowTitle;
+                if (hwnd != new IntPtr(0) && winn != string.Empty && hwnd != this.Handle)
+                    if (!defBlist.Contains(proc))
+                        lvListW.Items.Add(new ListViewItem(new string[] { winn, hwnd.ToString() }));
+            }
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            GetList();
+        }
+
+        private void btnAll_Click(object sender, EventArgs e)
+        {
+            Rectangle res = Screen.PrimaryScreen.Bounds;
+            foreach (ListViewItem item in lvListW.Items)
+            {
+                IntPtr hwnd = new IntPtr(int.Parse(item.SubItems[1].Text));
+                SetWindowPos(hwnd, -2, (res.Width / 2) - (GetControlSize(hwnd).Width / 2), (res.Height / 2) - (GetControlSize(hwnd).Height / 2), 0, 0, 0x1 | 0x10);
+            }
+            GetList();
+        }
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            GetList();
+        }
+
+        private void lvListW_DoubleClick(object sender, EventArgs e)
+        {
+            Rectangle res = Screen.PrimaryScreen.Bounds;
+            foreach (ListViewItem item in lvListW.Items)
+            {
+                if (item.Selected)
+                {
+                    IntPtr hwnd = new IntPtr(int.Parse(item.SubItems[1].Text));
+                    SetWindowPos(hwnd, -2, (res.Width / 2) - (GetControlSize(hwnd).Width / 2), (res.Height / 2) - (GetControlSize(hwnd).Height / 2), 0, 0, 0x1 | 0x10);
+                }
+            }
+        }
+
+        private void btnSelect_Click(object sender, EventArgs e)
+        {
+            Rectangle res = Screen.PrimaryScreen.Bounds;
+            foreach (ListViewItem item in lvListW.Items)
+            {
+                if (item.Selected)
+                {
+                    IntPtr hwnd = new IntPtr(int.Parse(item.SubItems[1].Text));
+                    SetWindowPos(hwnd, -2, (res.Width / 2) - (GetControlSize(hwnd).Width / 2), (res.Height / 2) - (GetControlSize(hwnd).Height / 2), 0, 0, 0x1 | 0x10);
+                }
+            }
+        }
+    }
+}
